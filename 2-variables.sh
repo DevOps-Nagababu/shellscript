10ae0fe79bd280c3f294a8d1299c300f609d77e2
Nagababu <nagababu.rachakonda@gmail.com>
2026-01-28 21:20:14 +0530

    Adding the file

---
+++ 2-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Values picking from Varaibles with Daynamic 
+
+PERSON1="Nagababu"
+PERSON2="Teja Sri"
+
+echo " Hello $PERSON2 how are you"
+echo " Hi $PERSON1 , I'm good how about you?"
+echo " Thanks for asking $PERSON2 , what are you doing?"
+echo "$PERSON1 Now i'm learing DevOps"
