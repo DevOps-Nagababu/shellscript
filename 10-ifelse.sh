9291dcfd66736db0164d07fae0b38ad43c65ecc1
Nagababu <nagababu.rachakonda@gmail.com>
2026-01-29 00:09:33 +0530

    Adding some code

---
+++ 10-ifelse.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+echo  "Please run this script with root user access"
+exit 1
+fi
+echo "installing Nginx"
+dnf install nginx -y
+
+if [ $? -ne 0]; then
+echo " installing nginx ------ FAILD"
+exit 1
+else
+echo "installing nginx --------SUCESS"
+fi
---
+++ 9-if.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUM=50
+
+if [ $NUM -gt 40 ] ; then
+echo "$NUM is greater than 40"
+fi
+
+if [ $NUM >= 50 ]; then
+echo "$NUM is greater than 50"
+fi
+
