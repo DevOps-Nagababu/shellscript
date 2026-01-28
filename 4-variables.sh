efe8aa13151056e2841621620952e1a21a9aa8c0
Nagababu <nagababu.rachakonda@gmail.com>
2026-01-28 21:48:19 +0530

    Adding the file

--- 4-variables.sh
+++ 4-variables.sh
@@ -6,3 +6,5 @@ echo "Please enter the user name: "
 read USER_NAME
 echo "Hello $USER_NAME"
 echo "Please enter the password :"
+read -s PASS # -s is hide the value entered from keyboard
+echo " Display password entered from keyboard : $PASS "  
