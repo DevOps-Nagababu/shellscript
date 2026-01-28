f2b027523e4476980c278b1bc6d3e842ad60bf0e
Nagababu <nagababu.rachakonda@gmail.com>
2026-01-28 21:32:16 +0530

    Adding the file

---
+++ 3-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# By using the specil variables arguments ($1,$2) we can pass variables during the run time
+
+PARAME1=$1
+PARAME2=$2
+
+echo "Hello $PARAME2 How are you ?"
+echo "Hi $PARAME1 I'm doing good , how about you?"
+echo "Hey $PARAME2 i'm also dong good ,what are you doing ?"
+echo  "$PARAME1 now i'm learing DevOps"
