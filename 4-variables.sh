c021796fb9983c429b76fa77084d62e317c777e4
Nagababu <nagababu.rachakonda@gmail.com>
2026-01-28 21:43:56 +0530

    Adding the file

---
+++ 4-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# User promt the during run time and values enter from keyboard
+
+echo "Please enter the user name: "
+read USER_NAME
+echo "Hello $USER_NAME"
+echo "Please enter the password :"
