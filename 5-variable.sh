f7b293ccfb56c417af5690bbb50df5a0e4794772
Nagababu <nagababu.rachakonda@gmail.com>
2026-01-28 22:24:30 +0530

    Adding the file

--- 5-variable.sh
+++ 5-variable.sh
@@ -2,6 +2,12 @@
 
 #I want  a command execute and take output into variable.
 
-VAR_NAME= $(date)
+SCRIPT_START_TIME=$(date +%s)
 
-echo "$VAR_NAME"
+echo "script start time is : $SCRIPT_START_TIME"
+sleep 10 
+SCRIPT_END_TIME=$(date +%s)
+echo "script end time is : $SCRIPT_END_TIME"
+
+DIFF_TIME=$(($SCRIPT_END_TIME-$SCRIPT_START_TIME))
+echo "script end time is : $DIFF_TIME"
