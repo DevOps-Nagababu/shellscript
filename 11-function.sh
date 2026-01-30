a6127fc9cb3dd388d1d1a4b960ff9e0dd7cb02c6
Nagababu <nagababu.rachakonda@gmail.com>
2026-01-30 12:39:13 +0530

    functions

--- 11-function.sh
+++ 11-function.sh
@@ -1,20 +1,26 @@
-#!bin/bash
+    #!bin/bash
 
-USERID=$(id -u)
+    USERID=$(id -u)
 
 
-    if [ $USERID -ne 0 ]; then
-        echo "Please login with root user and install the softwares"
-        exit 1
+        if [ $USERID -ne 0 ]; then
+            echo "Please login with root user and install the softwares"
+            exit 1
+        fi
+
+    VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 .... FAILURE"
+        else
+        echo "$2 .... SUCCESS"
     fi
+    }
 
-VALIDATE(){
-if [ $1 -ne 0 ]; then
-    echo "$2 .... FAILURE"
-    else
-    echo "$2 .... SUCCESS"
-fi
-}
+    dnf install nginx -y
+    VALIDATE $? installing nginx
 
-dnf install nginx -y
-VALIDATE $? installing nginx
+    dnf install nodejs -y
+    VALIDATE $? installing nodejs
+
+    dnf install mysql -y
+    VALIDATE $? installing mysql
