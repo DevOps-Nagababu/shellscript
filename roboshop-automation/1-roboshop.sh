2a688e1b8668b546f0335eb3a99b79e37d773b5f
Nagababu <nagababu.rachakonda@gmail.com>
2026-02-01 17:44:00 +0530

    adding file

--- roboshop-automation/1-roboshop.sh
+++ roboshop-automation/1-roboshop.sh
@@ -6,19 +6,19 @@ mkdir -p $LOG_FOLDER
 
 VALIDATION(){
     if [ $1 -ne 0 ]; then
-        echo " $2 ....FAILD"
+        echo " $2 ....FAILD" &>> $LOG_FILE
         exit 1
     else
-        echo " $2 ....SUCCESS"
+        echo " $2 ....SUCCESS" &>> $LOG_FILE
     fi
 }
 
 for packages in $@ 
 do
-    dnf list installed $packages
+    dnf list installed $packages &>> $LOG_FILE
     if [ $? -ne 0 ]; then
         echo "$packages not installed , now installing"
-        dnf install $packages -y
+        dnf install $packages -y &>> $LOG_FILE
         VALIDATION $? "$packages Installation "
     else
         echo "$packages are alredy installed"
