6ef5e6d688c01a59c70faa0cbfb67d67f3dd11c8
Nagababu <nagababu.rachakonda@gmail.com>
2026-02-01 16:27:35 +0530

    adding file

---
+++ roboshop-automation/1-roboshop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOG_FOLDER="/var/log/shell_script"
+LOG_FILE="$LOG_FOLDER/.$0.log"
+
+ if [ $USER_ID -ne 0 ]; then
+    echo "Please login to root user"
+    exit 1
+    
+fi
+
+VALIDATE(){
+   if [ $1 -ne 0 ]; then
+        echo "$2 Installation .....FAILD"
+    else
+        echo "$2 Installation......SUCCESS"
+    fi
+}
+
+dnf install nginx -y
+VALIDATE $? "installing nginx$
+
